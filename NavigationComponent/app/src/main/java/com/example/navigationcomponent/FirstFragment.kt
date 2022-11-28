f371fb2838d15a1ecc92bf15cfaa934cb1b10cbc
Rafael Fernandes <rafa.fernan10@gmail.com>
2022-11-28 20:53:05 -0300

    Navigation com Fragments - primeira parte

---
+++ NavigationComponent/app/src/main/java/com/example/navigationcomponent/FirstFragment.kt
@@ -0,0 +1,17 @@
+package com.example.navigationcomponent
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+
+class FirstFragment: Fragment(R.layout.fragment_first) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<TextView>(R.id.text_id).setOnClickListener{
+            findNavController().navigate(R.id.action_firstFragment_to_secondFragment)
+        }
+    }
+}
