01a28e5e0a3191e47c7420cbdf6a76fff83cd078
Rafael Fernandes <rafa.fernan10@gmail.com>
2022-12-05 00:12:43 -0300

    Navigation Graph

---
+++ andfun-kotlin-android-trivia-starter-code/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -0,0 +1,26 @@
+package com.example.android.navigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import com.example.android.navigation.databinding.FragmentTitleBinding
+
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding:FragmentTitleBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_title,container,false)
+        binding.playButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment))
+        return binding.root
+        //return inflater.inflate(R.layout.fragment_title, container, false) CÓDIGO ANTIGO
+    }
+}
